83bed28faa4da006bbd5fbfc6a722115b2246b65
Hugo Cruz <huggorviddal64@hotmail.com>
2017-09-07 12:36:37 -0300

    Códigos xamarin
    Soluções de problemas que podem acontecer no xamarin

---
+++ Loading-Xamarin.Forms-master/Loading/Loading/Loading/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Loading
+{
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            IsLoading = false;
+            BindingContext = this;
+        }
+        public async void Show(object sender, EventArgs e)
+        {
+            try
+            {
+                aparece();
+
+                //Chame sua função aqui
+                await Task.Delay(4000);
+
+
+                some();
+            }
+            catch (Exception ex)
+            {
+                some();
+                if (ex != null)
+                {
+                    //Trate seu erro aqui
+                }
+            }
+        }
+        public async void aparece()
+        {
+            IsLoading = true;
+        }
+        public async void some()
+        {
+            IsLoading = false;
+        }
+
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get
+            {
+                return this.isLoading;
+            }
+            set
+            {
+                this.isLoading = value;
+                RaisePropertyChanged("IsLoading");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
